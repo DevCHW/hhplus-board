deb2884983639fbf8040b7d27fc0518c086dd73e
DevCHW <ggoma003@naver.com>
2024-11-12 19:02:11 +0900

    first commit

---
+++ src/main/kotlin/com/hhplus/hhplusframeworkstudyboard/HhplusFrameworkStudyBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.hhplus.hhplusframeworkstudyboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusFrameworkStudyBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<HhplusFrameworkStudyBoardApplication>(*args)
+}
---
+++ src/test/kotlin/com/hhplus/hhplusframeworkstudyboard/HhplusFrameworkStudyBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hhplus.hhplusframeworkstudyboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusFrameworkStudyBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
